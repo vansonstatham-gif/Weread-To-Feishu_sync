e8ab081cc173fef77aaabf4acc6d0088a3b99404
Vanson Statham <vansonstatham@gmail.com>
2025-10-29 00:47:47 +0800

    Add environment variables for Weread and Feishu

---
+++ config/env.js
@@ -0,0 +1,5 @@
+WEREAD_COOKIE=wr_gid=279788753; wr_fp=2352408306; wr_ql=0; wr_vid=226129184; wr_skey=8FJLODwN; wr_pf=0; wr_rt=web%40Hj7ooNXiscsl2NW8jD1_AL; wr_localvid=e2b328f07d7a7520e2b3d39; wr_name=Vanson; wr_avatar=https%3A%2F%2Fres.weread.qq.com%2Fwravatar%2FWV0025-CirFk~aQYxmr2dSxN3Phie1%2F0; wr_gender=0
+FEISHU_APP_ID=你的飞书App ID
+FEISHU_APP_SECRET=quZT6bKZ5VS6gEEQW5YfjgOPaM6WONTi
+FEISHU_APP_TOKEN=cli_a870fb9b27f9100e
+FEISHU_TABLE_ID=OsD4bRf8WaQ9NFs49Vccer2Enpc
